6f2291fa9d3f041dd6edd3811694078d444d1d25
Fel1xgte <145865259+Fel1xgte@users.noreply.github.com>
2024-05-23 00:31:10 -0400

    Update Clonesets URLs and Terminologies

--- kubesphere-extension/extensions/kruise-game-dashboard/src/components/GameServerList/index.jsx
+++ kubesphere-extension/extensions/kruise-game-dashboard/src/components/GameServerList/index.jsx
@@ -22,8 +22,8 @@ function GameServerList(props) {
     const tableRef = useRef();
     const clusterId = params.name;
     const gsUrl = '/clusters/' + clusterId + '/kruise-game-dashboard/gameservers';
-    const url = '/clusters/' + clusterId + '/apis/game.kruise.io/v1alpha1/gameservers'
-    const gssDetailUrl = '/clusters/' + clusterId + '/customresources/gameservers.game.kruise.io/resources';
+    const url = '/clusters/' + clusterId + '/apis/apps.kruise.io/v1alpha1/clonesets`'
+    const gssDetailUrl = '/clusters/' + clusterId + '/customresources/clonesets.apps.kruise.io/resources';
 
     const navigate = useNavigate();
     const handleMenuItemClick = () => {
