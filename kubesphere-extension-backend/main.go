9d254a39a7c8311e1b8d1ee91c0e914bfda91ac8
Fel1xgte <145865259+Fel1xgte@users.noreply.github.com>
2024-05-23 00:28:00 -0400

    改成clonesets端口

--- kubesphere-extension-backend/main.go
+++ kubesphere-extension-backend/main.go
@@ -12,7 +12,7 @@ func main() {
 	v1 := r.Group("/kapis/kruise-game-api.kubesphere.io/v1alpha1")
 	{
 		v1.GET("/cluster/overview", handlers.GetSummary)
-		v1.GET("/gameserversets", handlers.GetGameServerSets)
+		v1.GET("/clonesets", handlers.GetGameServerSets) \\gameserversets对应clonesets，改端口
 		v1.GET("/gameservers", handlers.GetGameServers)
 		v1.GET("/gameserverset/:name", handlers.GetGameServerSetByName)
 		v1.GET("/gameserver/:name", handlers.GetGameServerByName)
